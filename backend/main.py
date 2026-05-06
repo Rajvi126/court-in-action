f86ce4b10cb83351bd097c5865d0705bf415c447
Rajvi126 <1032241191@tcetmumbai.in>
2026-05-06 22:48:35 +0530

    Prepare backend for deployment

--- backend/main.py
+++ backend/main.py
@@ -16,9 +16,10 @@ from utils.pdf_generator import generate_pdf
 # =============================
 # 📁 FILE SETUP
 # =============================
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DATA_DIR = "data"
-UPLOAD_DIR = "uploads"
+DATA_DIR = os.path.join(BASE_DIR, "data")
+UPLOAD_DIR = os.path.join(BASE_DIR, "uploads")
 
 os.makedirs(DATA_DIR, exist_ok=True)
 os.makedirs(UPLOAD_DIR, exist_ok=True)
